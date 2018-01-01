385e1dafaeebcf3b49b44177c360b506d6d8dddc
Tomarchelone <zolotuhinden@bk.ru>
2018-01-01 15:58:34 +0300

    Wrote pseudo-prime numbers generator

---
+++ target/debug/deps/time_lock_puzzles-6b1d2be35edaa56d.d
@@ -0,0 +1,5 @@
+/media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/target/debug/deps/time_lock_puzzles-6b1d2be35edaa56d: src/main.rs
+
+/media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/target/debug/deps/time_lock_puzzles-6b1d2be35edaa56d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/time_lock_puzzles.d
@@ -0,0 +1,1 @@
+/media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/target/debug/time_lock_puzzles: /media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/src/main.rs

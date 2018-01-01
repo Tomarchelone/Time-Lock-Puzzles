bc9ddfc363a6b709505d917120c22e3b54740002
Tomarchelone <zolotuhinden@bk.ru>
2018-01-01 16:43:26 +0300

    Primes aligned

--- target/debug/time_lock_puzzles.d
+++ target/debug/time_lock_puzzles.d
@@ -1,1 +1,1 @@
-/media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/target/debug/time_lock_puzzles: /media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/src/main.rs
+/media/tom/Windows/AllHere/Archives/Rust/time-lock-puzzles/target/debug/time_lock_puzzles: /media/tom/Windows/AllHere/Archives/Rust/time-lock-puzzles/src/main.rs

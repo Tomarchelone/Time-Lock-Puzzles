c4c742ddd2cd0ec0ec6c886b96aff10bc6f5685f
Tomarchelone <zolotuhinden@bk.ru>
2018-01-02 23:05:42 +0300

    First version of puzzler is written.

--- target/debug/deps/time_lock_puzzles-bb46b6481a3dd0c0.d
+++
@@ -1,5 +0,0 @@
-/media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/target/debug/deps/time_lock_puzzles-bb46b6481a3dd0c0: src/main.rs
-
-/media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/target/debug/deps/time_lock_puzzles-bb46b6481a3dd0c0.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/time_lock_puzzles-d737413363e16d0a.d
+++
@@ -1,5 +0,0 @@
-/media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/target/debug/deps/time_lock_puzzles-d737413363e16d0a: src/main.rs
-
-/media/tom/Windows/AllHere/Archives/Rust/time_lock_puzzles/target/debug/deps/time_lock_puzzles-d737413363e16d0a.d: src/main.rs
-
-src/main.rs:
